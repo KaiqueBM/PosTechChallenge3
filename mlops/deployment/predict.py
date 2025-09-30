594a6be37dbdab69b08175da75c25de8089691c5
pg.annajulia@gmail.com <pg.annajulia@gmail.com>
2025-09-30 22:32:35 +0000

    Escrevendo a tabela de predict

--- mlops/deployment/predict.py
+++ mlops/deployment/predict.py
@@ -2,21 +2,22 @@ import mlflow
 import pandas as pd
 from datetime import datetime
 
-def predict_batch(model_uri, input_path, output_path, model_version):
+def predict_batch(spark, model_uri, source_table, output_table, model_version):
     """
     Batch inference usando pandas e modelo scikit-learn registrado no MLflow.
     
     Args:
         model_uri: URI do modelo no MLflow (ex: models:/car_features_model/1)
-        input_path: caminho de entrada (CSV ou Delta -> aqui simplificamos para CSV)
-        output_path: caminho de saída (CSV ou Delta)
+        source_table: tabela da source
+        output_table: tabela dos resultados
         model_version: versão do modelo (para log)
     """
     # Carregar modelo
     model = mlflow.sklearn.load_model(model_uri)
 
     # Ler dataset
-    df = pd.read_csv(input_path)
+    df_raw = spark.read.table(source_table)
+    df = df_raw.toPandas()
 
     # Features (remove colunas alvo)
     X = df.drop(columns=["msrp", "city_mpg", "highway_mpg"], errors="ignore")
@@ -35,6 +36,7 @@ def predict_batch(model_uri, input_path, output_path, model_version):
     final_df["model_id"] = model_version
     final_df["timestamp"] = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
 
-    # Salvar em CSV
-    final_df.to_csv(output_path, index=False)
-    print(f"Predições salvas em: {output_path}")
+    # Salvar Resultados Na Tabela
+    spark_df = spark.createDataFrame(final_df)
+    spark.sql("create database if not exists postech_ml")
+    spark_df.write.mode("overwrite").saveAsTable(output_table)
